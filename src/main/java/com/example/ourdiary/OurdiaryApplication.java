bea5aa43d3d69f5e185373d795ced5a6452b4fd9
안준헌 <frozengrape@mz.co.kr>
2023-06-29 16:09:20 +0900

    ourdiary init

---
+++ src/main/java/com/example/ourdiary/OurdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurdiaryApplication.class, args);
+    }
+
+}
